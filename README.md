31ae2766d59f7fac808ad926cc676535a0f0c3b6
AntzUhl <antzuhl1998@gmail.com>
2020-09-27 00:29:03 +0800

    feature(conf/code): 初步功能完成

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # main-to-master
-master branch更名为main后照样显示master
+
+硬核的将main修改显示为master的chrome插件。
+
+##### TODO
+
+1. 自动跳转到仓库到master分支
---
+++ js/cat-th.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+
+  var get = $(".btn .css-truncate-target").innerText
+  if (get === 'main') {
+    $(".btn .css-truncate-target").innerText = 'master'
+  }
+
+})();
